3429fbeeb7171268279768a34604c46853ee32b8
Raphael Ndwiga <raphndwi@gmail.com>
2020-07-10 10:17:19 +0300

    added sequelize support for mysql, mariadb, postgres, mssql support

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { Sequelize } = require('sequelize');
 const helmet = require('helmet')
 const compression = require('compression')
  const userRoutes = require('./api/users/users.controller');
@@ -23,6 +24,22 @@ app.use(helmet())
 .catch(err => {
     console.log(Error, err.message);
 }); */
+
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect:'mariadb', /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+    dialectOptions: {
+        timezone: 'Etc/GMT-3',
+      },
+  });
+
+
+  try {
+     sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 
 app.use(morgan('dev'));
